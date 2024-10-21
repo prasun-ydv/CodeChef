5b69a5b46018077077fb036ad4ea0c03e8899158
prasun-ydv <110124180+prasun-ydv@users.noreply.github.com>
2024-10-21 22:01:09 +0530

    Add files via upload

---
+++ AIRINDEX.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t ;
+	if( t <100)
+	cout << "YES" << endl ;
+	else cout << "NO" << endl ; 
+	return 0;
+}
---
+++ SWEETSHOP.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,x;
+	cin>>x>>n;
+	int diff=x-10*n;
+	int numberJalebi=diff/20;
+	cout<<numberJalebi<<endl;
+}
